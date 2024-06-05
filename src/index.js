5bb4c8ab1215796ced2cff7e978689f617ee3283
latin bubel <esterapietkiewicz0110@gmail.com>
2024-06-05 15:42:08 +0200

    Added form handling and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#recipe", {
+    strings: "recipe is here",
+    autoStart: true,
+    delay: 1.5,
+    cursor: "",
+  });
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
